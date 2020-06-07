24bd36c9844f21623669e091281c9dd76a3975ad
Rogerio Caetano <cataua@gmail.com>
2020-06-07 18:41:42 -0300

    criado dir backend_api

---
+++ backend-api/docker-machine-prompt.bash
@@ -0,0 +1,4 @@
+base=https://github.com/docker/machine/releases/download/v0.16.2 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
+  chmod +x /usr/local/bin/docker-machine
