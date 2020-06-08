1773107b7535930e7db69a9975cd714250e84330
hedenica <hedenica@hotmail.com>
2020-06-08 09:43:50 -0300

    ✨  button component | signup component | adjusting styles | add react-grid lib

--- frontend/src/components/Login/index.tsx
+++ frontend/src/components/Login/index.tsx
@@ -10,11 +10,11 @@ const Login = () => {
   return (
     <>
       <div className="login-box">
-        <h2>Login</h2>
+        <h2>Faça seu login</h2>
         <form>
           <div className="user-box">
-            <input type="text" name="" required />
-            <label>Usuário</label>
+            <input type="email" name="" required />
+            <label>E-mail</label>
           </div>
       <div className="user-box">
         <input type="password" name="" required/>
@@ -26,7 +26,7 @@ const Login = () => {
           <span></span>
           <span></span>
           <span></span>
-        Submit
+        ENTRAR
       </a>
       </div>
       </form>
