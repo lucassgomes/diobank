8c5b2a39fc06e7c5c5d56955d5b935723d8ded4c
Rogerio Caetano <cataua@gmail.com>
2020-06-08 17:54:47 -0300

    ajuste docker-compose

--- backend-api/docker-machine-prompt.bash
+++
@@ -1,4 +0,0 @@
-base=https://github.com/docker/machine/releases/download/v0.16.2 &&
-  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
-  sudo mv /tmp/docker-machine /usr/local/bin/docker-machine &&
-  chmod +x /usr/local/bin/docker-machine
