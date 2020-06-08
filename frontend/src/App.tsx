7f72df2a03fc2b633a7f58c536a3e58fbdbc9944
Pablo Gomes <pablojr17@gmail.com>
2020-06-08 15:18:32 -0300

    🎸️ Home DioBank

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import LandingPage from './pages/LandingPage/index';
+//import LandingPage from './pages/LandingPage/index';
+import Home from './pages/Home';
+
 
 import './App.scss';
 
 function App() {
   return (
-    <LandingPage />
+    //<LandingPage />
+    <Home />
   )
 };
 
---
+++ frontend/src/components/HeaderHome/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {FiBell} from 'react-icons/fi'
+
+
+import './styles.scss'
+import Logo from '../../assets/img/dio.png';
+
+const HeaderHome = () => {
+  return (
+    <header>
+    <div className="head">
+      <img src={Logo} alt=""/>
+      <FiBell color={"#FCA024"} size={32} />
+    </div>
+  
+      <nav className="site-nav">
+        <ul>
+          <li className="active"><a href="#0">Atividades</a></li>
+          <li><a href="#0">Pagamentos</a></li>
+          <li><a href="#0">Carteira</a></li>
+          <li><a href="#0">Sair</a></li>
+        </ul>
+    </nav>
+  
+  </header>
+    )
+};
+
+export default HeaderHome;
