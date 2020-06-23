9a668d7ac665e93c06e84079a6dc73fc85628b03
brenokf <breno-kf@hotmail.com>
2020-06-23 09:58:17 -0400

    add app.use(routes) não tinha

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -5,11 +5,14 @@ import './database';
 import cors from 'cors';
 
 import express,{Response, Request, NextFunction} from 'express';
+import routes from './routes/index';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
+
 
 app.listen(3333,()=> {
   console.log('🏦 Server started on port: 3333');
