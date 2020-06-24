6a8c989b87ca5a7c6e7aed6b1659f00d6f9e0c28
brenokf <breno-kf@hotmail.com>
2020-06-23 21:06:07 -0400

    add route session

--- BackEnd/src/routes/index.ts
+++ BackEnd/src/routes/index.ts
@@ -1,9 +1,11 @@
 import Routes  from 'express';
 import usersRouter from './users.routes';
+import sessionRouter from './sessions.routes';
 
 
 const routes = Routes();
 
 routes.use('/users',usersRouter);
+routes.use('/sessions',sessionRouter);
 
 export default routes;
