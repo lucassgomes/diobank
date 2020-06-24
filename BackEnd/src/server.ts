37e29c9e0addc7e41007cb6950d80b2eb6f3cf36
brenokf <breno-kf@hotmail.com>
2020-06-23 21:05:36 -0400

    retirada import repetido

--- BackEnd/src/server.ts
+++ BackEnd/src/server.ts
@@ -3,7 +3,6 @@ import 'express-async-errors'
 import './database';
 
 import cors from 'cors';
-import routes from './routes/index';
 
 import express,{Response, Request, NextFunction} from 'express';
 import routes from './routes/index';
